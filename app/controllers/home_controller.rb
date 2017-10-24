d595ba58aa33edec45b2cd4ee4534e7c7f83da1e
John Elder <johne4196@gmail.com>
2017-10-24 20:12:39 +0000

    modified gemfile for pg

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
+    if params[:id] == ""
+      @nothing = "Hey, you forgot to enter a symbol"
+    elsif
+    
+      if params[:id]
+        @stock = StockQuote::Stock.quote(params[:id])
+      end
+    end
   end
+  
   
   def about
   end
